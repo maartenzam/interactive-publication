8140faf78c510866752c11c4dc66e885e740525c
Maarten <maartenzam@gmail.com>
2023-02-01 22:17:23 +0100

    Bug fixes and visualisation

---
+++ src/routes/vis/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+	import pages from '$lib/data/pages.json';
+	import ForceGraph from '$lib/components/ForceGraph.svelte';
+	import calculateGraph from '$lib/utils/calculateGraph.js'
+</script>
+
+<main>
+	<h1>Visualisation</h1>
+	<ForceGraph data={calculateGraph(pages, 3, true)} />
+</main>
